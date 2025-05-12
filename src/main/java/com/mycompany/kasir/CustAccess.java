11406a9a067ba0f02050b048fe7178d8da1df4fb
ZFL01 <muhammadzaiful88@gmail.com>
2025-05-12 19:30:46 +0700

    Login

--- src/main/java/com/mycompany/kasir/CustAccess.java
+++ src/main/java/com/mycompany/kasir/CustAccess.java
@@ -16,42 +16,108 @@ public class CustAccess extends javax.swing.JFrame {
     private void initComponents() {
 
         Panel = new javax.swing.JPanel();
-        Kembali = new javax.swing.JButton();
+        panelRound2 = new komponen.PanelRound();
+        panelRound1 = new komponen.PanelRound();
+        panelRound3 = new komponen.PanelRound();
         IniPanel = new javax.swing.JPanel();
+        Kembali = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Pemesanan - Pelanggan");
-        setMaximumSize(new java.awt.Dimension(1440, 900));
         setMinimumSize(new java.awt.Dimension(1440, 900));
-        setPreferredSize(new java.awt.Dimension(1440, 900));
         setResizable(false);
 
         Panel.setLayout(null);
 
-        Kembali.setBackground(new java.awt.Color(0, 153, 255));
+        panelRound2.setBackground(new java.awt.Color(51, 153, 255));
+
+        panelRound1.setBackground(new java.awt.Color(51, 204, 255));
+        panelRound1.setRoundBottomLeft(50);
+        panelRound1.setRoundBottomRight(50);
+        panelRound1.setRoundTopLeft(50);
+        panelRound1.setRoundTopRight(50);
+
+        panelRound3.setBackground(new java.awt.Color(102, 204, 255));
+        panelRound3.setRoundBottomLeft(50);
+        panelRound3.setRoundBottomRight(50);
+        panelRound3.setRoundTopLeft(50);
+        panelRound3.setRoundTopRight(50);
+
+        IniPanel.setBackground(new java.awt.Color(255, 255, 255));
+        IniPanel.setMaximumSize(new java.awt.Dimension(1240, 640));
+        IniPanel.setMinimumSize(new java.awt.Dimension(1240, 640));
+        IniPanel.setPreferredSize(new java.awt.Dimension(1240, 640));
+        IniPanel.setLayout(new java.awt.BorderLayout());
+
+        Kembali.setBackground(new java.awt.Color(255, 102, 102));
         Kembali.setText("Kembali");
         Kembali.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 KembaliActionPerformed(evt);
             }
         });
-        Panel.add(Kembali);
-        Kembali.setBounds(50, 818, 150, 50);
+        IniPanel.add(Kembali, java.awt.BorderLayout.PAGE_END);
 
-        IniPanel.setBackground(new java.awt.Color(255, 102, 102));
-        IniPanel.setMaximumSize(new java.awt.Dimension(1240, 640));
-        IniPanel.setMinimumSize(new java.awt.Dimension(1240, 640));
-        IniPanel.setPreferredSize(new java.awt.Dimension(1240, 640));
-        IniPanel.setLayout(new java.awt.BorderLayout());
-        Panel.add(IniPanel);
-        IniPanel.setBounds(60, 120, 1240, 640);
+        javax.swing.GroupLayout panelRound3Layout = new javax.swing.GroupLayout(panelRound3);
+        panelRound3.setLayout(panelRound3Layout);
+        panelRound3Layout.setHorizontalGroup(
+            panelRound3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelRound3Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(IniPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40))
+        );
+        panelRound3Layout.setVerticalGroup(
+            panelRound3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelRound3Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(IniPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40))
+        );
+
+        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
+        panelRound1.setLayout(panelRound1Layout);
+        panelRound1Layout.setHorizontalGroup(
+            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelRound1Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(panelRound3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40))
+        );
+        panelRound1Layout.setVerticalGroup(
+            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelRound1Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(panelRound3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40))
+        );
+
+        javax.swing.GroupLayout panelRound2Layout = new javax.swing.GroupLayout(panelRound2);
+        panelRound2.setLayout(panelRound2Layout);
+        panelRound2Layout.setHorizontalGroup(
+            panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelRound2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(94, Short.MAX_VALUE))
+        );
+        panelRound2Layout.setVerticalGroup(
+            panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelRound1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31))
+        );
+
+        Panel.add(panelRound2);
+        panelRound2.setBounds(0, 0, 1500, 870);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(Panel, javax.swing.GroupLayout.DEFAULT_SIZE, 1434, Short.MAX_VALUE)
+                .addComponent(Panel, javax.swing.GroupLayout.DEFAULT_SIZE, 1669, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -71,5 +137,8 @@ public class CustAccess extends javax.swing.JFrame {
     private javax.swing.JPanel IniPanel;
     private javax.swing.JButton Kembali;
     private javax.swing.JPanel Panel;
+    private komponen.PanelRound panelRound1;
+    private komponen.PanelRound panelRound2;
+    private komponen.PanelRound panelRound3;
     // End of variables declaration//GEN-END:variables
 }
